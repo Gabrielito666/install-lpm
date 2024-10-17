b66227579495f99b768cd1851ffacb8f88fd51a9
Gabrielito666 <galileo.fandango@gmail.com>
2024-10-17 14:59:14 -0300

    debug6

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
 #!/usr/bin/env node
-var{spawnSync:l}=require("child_process"),n=require("fs"),o=require("path"),c=s=>o.resolve(process.env.HOME,s),i=s=>o.resolve(__dirname,s),e={ROOT_DIR:c(".lpm/"),LIB_DIR:c(".lpm/lib/"),PACKAGES_DIR:c(".lpm/packages/"),DATABASE_DIR:c(".lpm/lib/data.db"),INDEX_DIR:c(".lpm/lib/index.js"),PACKAGE_JSON_DIR:c(".lpm/lib/package.json"),BASHRC_DIR:c(".bashrc"),INDEX_SRC:i("src/index.js"),PKG_JSON_SRC:i("src/package.json")};n.mkdirSync(e.ROOT_DIR);n.mkdirSync(e.LIB_DIR);n.mkdirSync(e.PACKAGES_DIR);var a=n.readFileSync(e.INDEX_SRC,"utf-8"),t=n.readFileSync(e.PKG_JSON_SRC,"utf-8");n.writeFileSync(e.INDEX_DIR,a,"utf-8");n.writeFileSync(e.PACKAGE_JSON_DIR,t,"utf-8");l("cd ~/.lpm/lib/ && npm install && cd ~");var S=`
+const { spawnSync } = require("child_process");
+const fs = require("fs");
+const path = require("path");
+
+const $home = (p) => path.resolve(process.env.HOME, p);
+const $dir = p => path.resolve(__dirname, p);
+
+const CONSTS =
+{
+    ROOT_DIR : $home('.lpm/'),
+    LIB_DIR : $home('.lpm/lib/'),
+    PACKAGES_DIR : $home('.lpm/packages/'),
+    DATABASE_DIR : $home('.lpm/lib/data.db'),
+    INDEX_DIR : $home('.lpm/lib/index.js'),
+    PACKAGE_JSON_DIR : $home('.lpm/lib/package.json'),
+    BASHRC_DIR : $home('.bashrc'),
+    LPM_TGZ_PATH : $dir('lpm.tgz')
+}
+
+fs.mkdirSync(CONSTS.ROOT_DIR);
+fs.mkdirSync(CONSTS.LIB_DIR);
+fs.mkdirSync(CONSTS.PACKAGES_DIR);
+
+fs.copyFileSync(CONSTS.LPM_TGZ_PATH, LIB_DIR);
+
+spawnSync("cd ~/.lpm/lib/ && tar -xzvf lpm.tgz && rm lpm.tgz && npm install && cd ~");
+
+const nuevaLinea = `
 lpm()
 {
     node ~/.lpm/lib/index.js $1 $2 $3 $4
-}`;n.appendFileSync(e.BASHRC_DIR,S,"utf-8");l("source ~/.bashrc");console.log("lpm ha sido instalado");
+}`;
+
+fs.appendFileSync(CONSTS.BASHRC_DIR, nuevaLinea, "utf-8");
+spawnSync("source ~/.bashrc");
+
+console.log("lpm ha sido instalado");
